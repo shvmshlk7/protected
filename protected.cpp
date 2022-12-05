c1fd8082bf47738cbf0784e9a1d0c8e6a977b233
SHIVAM SHELKE <103065763+shvmshlk7@users.noreply.github.com>
2022-12-05 07:53:14 -0800

    Create protected.cpp

---
+++ protected.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class base{
+    protected:
+        int a;
+    private:
+        int b;
+};
+
+class derived:protected base{
+
+};
+
+int main(){
+    base b;
+    derived d;
+    //cout<<d.a;   // will not work since a is protected in both base as well as derived class  
+    return 0;
+}
